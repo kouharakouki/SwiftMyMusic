e48d6021827f509f886d2936fc29493bed8c613d
koukikouhara <koki.kohara916@gmail.com>
2021-04-20 15:40:32 +0900

    Initial Commit

---
+++ SwiftMyMusic/SwiftMyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMyMusicApp.swift
+//  SwiftMyMusic
+//
+//  Created by 鴻原航輝 on 2021/04/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
